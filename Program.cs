00f1fc91aaab9cbedd158ffb0da901151d8b56a8
Antoniocns <acnspeixe@gmail.com>
2021-08-14 20:19:24 -0300

    Ufa acabei

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OlaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ola usuario, eu me chamo antonio carlos!");
+        }
+    }
+}
